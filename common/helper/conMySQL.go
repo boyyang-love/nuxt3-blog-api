ba94a42e7583cf76fce44b291930b18da71bb753
boyyang <boyyang-love@outlook.com>
2024-04-02 08:28:35 +0800

    feat: 修改接口 :sparkles:
    修改接口

--- common/helper/conMySQL.go
+++ common/helper/conMySQL.go
@@ -2,6 +2,7 @@ package helper
 
 import (
 	"blog_backend/internal/config"
+	"blog_backend/models"
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -36,12 +37,12 @@ func ConMySQL(mySQLConf config.MySQLConf) (db *gorm.DB, err error) {
 func AutoMigrate(db *gorm.DB) (err error) {
 
 	err = db.AutoMigrate(
-	//&models.BaseComment{},
-	//&models.User{},
-	//&models.Article{},
-	//&models.Tag{},
-	//&models.Image{},
-	//&models.Upload{},
+		&models.BaseComment{},
+		&models.User{},
+		&models.Article{},
+		&models.Tag{},
+		&models.Image{},
+		&models.Upload{},
 	)
 
 	return err
